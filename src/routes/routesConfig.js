471b8b9cea9d66a41dc8e67f331678d1cad76019
Dzmitry Smirnou <nirverem@gmail.com>
2023-07-28 15:06:03 +0300

    sls

--- src/routes/routesConfig.js
+++ src/routes/routesConfig.js
@@ -8,7 +8,7 @@ import ErrorMessage from '../components/ErrorMessage/ErrorMessage';
 
 const routesConfig = [
     {
-        path: '/',
+        path: '/Star-Wars',
         element: <HomePage/>,
         errorElement: <NotFoundPage/>
     },
